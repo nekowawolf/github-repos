063b04d6ae53603cb450aa9550a8a74d1e003cb0
nekowawolf <irukainu20@gmail.com>
2026-09-08 01:51:10 +0700

    refactor(ui): improve search bar styling and replace svg icon

--- src/app/directory/GithubReposContent.tsx
+++ src/app/directory/GithubReposContent.tsx
@@ -9,7 +9,7 @@ import { FallbackImage } from '@/components/FallbackImage';
 import { Suspense, useRef, useState, useEffect } from 'react';
 import { FaCode, FaServer, FaDatabase, FaShieldAlt, FaGraduationCap, FaGithub, FaRegFileImage, FaFileAlt } from 'react-icons/fa';
 import { LuAudioLines } from "react-icons/lu";
-import { GoCpu } from "react-icons/go";
+import { GoCpu, GoSearch } from "react-icons/go";
 import { RiRobot2Line } from "react-icons/ri";
 import { MdOutlineDesignServices, MdOutlineOndemandVideo } from "react-icons/md";
 import { CgClose } from "react-icons/cg";
@@ -161,17 +161,13 @@ function GithubReposContentInner() {
 
                 {/* Search Bar */}
                 <div className="w-full max-w-xl mb-6 relative">
-                    <div className="absolute left-4 top-1/2 -translate-y-1/2 text-fill-color/50">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                        </svg>
-                    </div>
+                    <GoSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-fill-color w-5 h-5" />
                     <input
                         type="text"
                         placeholder="Search Repositories"
                         value={localSearchQuery}
                         onChange={handleSearchChange}
-                        className="w-full py-3 pl-12 pr-12 rounded-full card-color border border-color focus:outline-none focus:border-blue-500 text-fill-color placeholder:text-fill-color/50 transition-colors"
+                        className="w-full py-3 pl-12 pr-12 rounded-full card-color border border-color focus:outline-none focus:ring-2 focus:ring-blue-600/80 focus:border-blue-600 text-fill-color placeholder:text-fill-color/50 transition-colors"
                     />
                     {localSearchQuery && (
                         <button
